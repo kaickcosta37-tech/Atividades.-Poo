f065292fef2e501497b5689b87e0279e4325a7f7
kaickcosta37-tech <kaickcosta37@gmail.com>
2026-05-22 08:18:50 -0300

    Update teste.poo.java

--- teste.poo.java
+++ teste.poo.java
@@ -1,26 +1,39 @@
-public class Main {
+class Aluno {
+    // Atributos
+    String  nome;
+    double nota1, nota2;
 
-    public static void main(String[] args) {
-
-        String modelo = "Ferrari";
-
-        // ---------------------------------------------------------
-        // ESCOPO DE MÉTODO
-        // ---------------------------------------------------------
-
-        String mensagem = "Detalhes do carro:";
-
-        // ---------------------------------------------------------
-        // ESCOPO DE BLOCO
-        // ---------------------------------------------------------
-
-        if (modelo != null) {
-
-            boolean temModelo = true;
-
-            System.out.println(mensagem + " Modelo " + modelo);
-
-            System.out.println("Tem modelo? " + temModelo);
-        }
+    // Construtor
+    Aluno(String n, double n1, double n2){
+        nome = n;
+        nota1 = n1;  nota2 = n2;
     }
+
+    double calcularMedia() {
+        return (nota1 + nota2) / 2;
+    }
+
+    void apresentar() {
+        System.out.println(nome + ": " + calcularMedia());
+    }
+}
+
+class Main {
+  public static void main(String[] args){
+
+    // Criando objetos
+    Aluno a1 = new Aluno("João", 8.0, 7.5);
+    Aluno a2 = new Aluno("Maria", 9.5, 10.0);
+
+    // Acessando atributos
+    System.out.println(a1.nome);
+    System.out.println(a2.nota1);
+
+    // Chamando métodos
+    a1.apresentar();
+    a2.apresentar();
+
+    double m = a1.calcularMedia();
+    System.out.println("Média: " + m);
+  }
 }
